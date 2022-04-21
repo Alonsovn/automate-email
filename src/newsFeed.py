2224722f507e27073ef6ff62049f62c3b9ebf50b
Alonsovn <alonsonh94@gmail.com>
2022-04-20 21:00:23 -0600

    Remove dependencies versions

---
+++ src/newsFeed.py
@@ -0,0 +1,29 @@
+# NewsApi key: be59b26e45d2472da851b555881647ac
+import requests
+
+
+class NewsFeed:
+    base_url = "https://newsapi.org/v2/everything?"
+    API_KEY = "be59b26e45d2472da851b555881647ac"
+
+    def __init__(self, interest, date_from, date_to, language):
+        self.interest = interest
+        self.date_from = date_from
+        self.date_to = date_to
+        self.language = language
+
+    def get(self):
+        response = requests.get(
+            f'{self.base_url}qInTitle={self.interest}&'
+            f'from={self.date_from}&to={self.date_to}&'
+            f'language={self.language}&'
+            f'apiKey={self.API_KEY}')
+
+        content = response.json()
+        articles = content["articles"]
+
+        email_body = ""
+        for article in articles:
+            email_body += f" {article['title']}, \n {article['url']}\n\n"
+
+        return email_body
