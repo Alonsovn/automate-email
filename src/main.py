1a00fc9479a65638cb438a15440e1b9a26972792
Alonsovn <alonsonh94@gmail.com>
2022-04-20 21:02:12 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
-print("Hello! I'm a template project")
+from src.newsFeed import NewsFeed
+
+
+def main():
+    interest = "tesla"
+    date_from = "2022-03-20"
+    date_to = "2022-04-20"
+    language = "en"
+
+    n = NewsFeed(interest, date_from, date_to, language)
+    print(n.get())
+
+
+main()
