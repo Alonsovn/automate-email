1ee74bbee5b79e3139ca8150d183cbeac3ec18b5
Alonsovn <alonsonh94@gmail.com>
2022-04-21 19:25:08 -0600

    Send emails from main

--- src/email_file.py
+++ src/email_file.py
@@ -5,16 +5,18 @@ import yagmail
 
 from src.newsFeed import NewsFeed
 
-df = pd.read_excel("../people.xlsx")
-date_from = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-date_to = datetime.datetime.now().strftime("%Y-%m-%d")
 
-for index, row in df.iterrows():
-    news_feed = NewsFeed(interest=row['interest'], date_from=date_from,
-                         date_to=date_to)
-    email = yagmail.SMTP(user="alonsonh94@gmail.com",
-                         password="pwd")
-    email.send(to=row['email'],
-               subject=f"Your {row['interest']} news for today!",
-               contents=f"Hi {row['name']}\n See what's on about {row['interest']} today."
-                        f"\n {news_feed.get()} \nRegards")
+def send_email():
+    df = pd.read_excel("people.xlsx")
+    date_from = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    date_to = datetime.datetime.now().strftime("%Y-%m-%d")
+
+    for index, row in df.iterrows():
+        news_feed = NewsFeed(interest=row['interest'], date_from=date_from,
+                             date_to=date_to)
+        email = yagmail.SMTP(user="alonsonh94@gmail.com",
+                             password="fluidrnjqkpktrvt")
+        email.send(to=row['email'],
+                   subject=f"Your {row['interest']} news for today!",
+                   contents=f"Hi {row['name']}\n See what's on about {row['interest']} today."
+                            f"\n {news_feed.get()} \nRegards")
--- src/main.py
+++ src/main.py
@@ -1,14 +1,9 @@
+from src.email_file import send_email
 from src.newsFeed import NewsFeed
 
 
 def main():
-    interest = "tesla"
-    date_from = "2022-03-20"
-    date_to = "2022-04-20"
-    language = "en"
-
-    n = NewsFeed(interest, date_from, date_to, language)
-    print(n.get())
+    send_email()
 
 
 main()
